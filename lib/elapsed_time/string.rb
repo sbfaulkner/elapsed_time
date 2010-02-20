19788c9517a028ee36a3565c8cbfca66423e4f91
S. Brent Faulkner <brent@mis-bfaulkner.local>
2010-02-20 09:48:00 -0500

    handle assignment of numeric values for units other than seconds

--- lib/elapsed_time/string.rb
+++ lib/elapsed_time/string.rb
@@ -11,6 +11,7 @@ module ElapsedTime
 
       hours_per_day = options[:hours_per_day] || 24
       unit_of_time = options[:unit] || :seconds
+      raise(ArgumentError, "unit should be one of :days, :hours, :minutes or :seconds; got #{unit_of_time}") unless %w(days hours minutes seconds).include?(unit_of_time.to_s)
 
       scanner = StringScanner.new self.strip.downcase.gsub(/(\s+and\s+|\s*,\s*)/, ' ')
 
@@ -22,7 +23,7 @@ module ElapsedTime
         if scanner.scan(/([a-z]+)\s*/)
           case unit = scanner[1]
           when /\Ad(ay(s)?)?\Z/
-            duration = duration.days.to_i
+            duration = (duration * hours_per_day.hours).to_i
           when /\Ah(our(s)?)?\Z/
             duration = duration.hours.to_i
           when /\Am(in(ute(s)?)?)?\Z/
@@ -34,7 +35,16 @@ module ElapsedTime
           end
         else
           raise(ArgumentError, "unrecognized format for elapsed time") if scanner.scan(/./)
-          duration = duration.to_i
+          case unit_of_time
+          when :days
+            duration = (duration * hours_per_day.hours).to_i
+          when :hours
+            duration = duration.hours.to_i
+          when :minutes
+            duration = duration.minutes.to_i
+          when :seconds
+            duration = duration.to_i
+          end
         end
 
         elapsed_time = (elapsed_time || 0) + duration
@@ -51,8 +61,6 @@ module ElapsedTime
         elapsed_time /= 1.minute.to_f
       when :seconds
         elapsed_time
-      else
-        raise(ArgumentError, "unit should be one of :days, :hours, :minutes or :seconds; got #{unit_of_time}")
       end
       elapsed_time.round
     end
